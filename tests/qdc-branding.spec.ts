47dad30c8e4364f206f9dfbb2dc8ec0e9b088644
Ahmed Hussein <ahmedhussein.developer@gmail.com>
2026-08-10 19:00:06 +0300

    [QF-5959] Optimize QDC Open Graph image payloads

--- tests/qdc-branding.spec.ts
+++ tests/qdc-branding.spec.ts
@@ -4,46 +4,48 @@ import fs from 'fs';
 import path from 'path';
 
 const PREMADE_SHA256: Record<string, string> = {
-  'og-ar.png': '09b0105374b97d0f546ad51b943809ac5376be8ea0e51c31e490e27dd6236f55',
-  'og-bn.png': '4fd21d0680b51e90326247130c0fb988fcad08296cf54241e3050d46f7199bd0',
-  'og-en.png': 'a77f1c652dcbb9349b2d9bc491c4c4a7cf2870ab9d2f834074fe8712a7044ffd',
-  'og-es.png': 'ef98e3909ccb25e807e3e3087750d16410818784ebbe831f6c6516d43ed2ce8b',
-  'og-fa.png': 'a8b79572b16661e9b8b40329a1a44dd4d3d4e7fd29a6128bd12ec92340a8f4ff',
-  'og-fr.png': '41d20f20df79eadf8cee371dccdf83d38898ef1c4386e987561ab4f95dc01792',
-  'og-id.png': 'c71783bf6f499f632584c3ab4a2679341963a261afeb359a499316c745fa7942',
-  'og-ms.png': '57e10c140185ad9670948fe72303c998209b28bd3f2ab4975f2e84ce0283b388',
-  'og-nl.png': '8c5dd412011bfe4f8a0c7f410fc58d6e7d33119fea55881f5c93aee7c55fa326',
-  'og-sw.png': 'd84f1a258bb63de238fdc22f7bb82f698cbfe6eb1535cb2673331a880ac33528',
-  'og-tr.png': 'a4801386ed021c033f0922b165cbe2ce3503ce8f1397d1e2ac1c44a366bd34cd',
-  'og-ur.png': 'c5516589bd28e4d38fee85ae824438e8ad8510f1f71d45a7c9611088455c2a54',
-  'og-vi.png': '2ed847194bd6c49ed3dfd161b08041274800f5a009510d87bab6fb4a33e00d01',
-  'og_about_en.png': 'bfa3f04175658597167b1fb97be5969a971fa6d2cbd9352b631ec91080a3a28e',
-  'og_beyond_ramadan.png': '366bc1a58bc9a537932c29b36503bbe3fff5f93e3bd2cc387e686f1bbcd1b60a',
-  'og_calendar.png': 'a2e00745a803a0e33eef3129677fb432d8b7cc947cee20a003054f90af25e5f1',
-  'og_daily.png': 'f1bc0c4029fc83303a37dd9350e515db5685df2c844a1191c23921899b1efd15',
-  'og_embed.png': '56c6e631cb0ca14f58eb74e20c5bb57df0bd2d52c644a3a06f8be0e8d52e79ea',
-  'og_explore_answers.png': 'b18e6167182ba1fc972eb1507f04813a864808175689c20f3b639f0c06b5b677',
-  'og_learning_plans.png': '309ea8d2a3433b275bc1724cac3d0379d875b784c3e7bafa3ddb3ce2ab42eeac',
-  'og_media.png': 'eff5b4c81572b8ea3b452d767f2bbed4c06132d4555525d11aa47099b282e2f4',
-  'og_preparing_for_ramadan.png': '4ab4f1c5b5e4424240271922a38df754ee5b86a2a8f0ca7162028053bb426780',
+  'og-ar.png': '6ddaab78c8a978396c933340f29adb021073d231f3d205626f9133db2fa4e987',
+  'og-bn.png': '5b4d25627d9f0a107984b6d902ae45d09769553f37e9d8bc35c9829f9fc1875d',
+  'og-en.png': '9558f4b232c009a642bfcd703da26663af001fd3b8194f7bc3fbc4a4e73302b2',
+  'og-es.png': '729c4763c858e089dc65f8a1d7a998938ce53df654d81906e106710a7929a16b',
+  'og-fa.png': 'dfaf4a954bf5f58674ea9612f93e5b0c22b71863e9037b9cbee66c5ffd0a66db',
+  'og-fr.png': '943f64ca3cfc549b065ab1d2c0851f0eb6a7b2db463415df87a5b5807f63c4bd',
+  'og-id.png': 'd5f094b527a3c55d1a4e9ba0604d447b2b106f5ee47fe9559f1733aea72e9b8f',
+  'og-ms.png': '710589bd3197017dc64335fdfd95f693b18ca39b2e41a74fab2b697f62a50560',
+  'og-nl.png': '2749ecb782bd3ca26a005f5589ca9c10f306b9782c68f6b706d08beb52b8caa5',
+  'og-sw.png': '765728128ff07bf31a81e0ee0aa105ae5e444ae2bb4392aabf70015ffbbe10ca',
+  'og-tr.png': '38e17f62a62bfcd1c2037bae732e3d849ae76968f00cb9dc64c3bde1766a0b3d',
+  'og-ur.png': 'b352971ed1101f1c4307d5faece63009426f38331a7c5e06e8676077f6941e96',
+  'og-vi.png': '19f9c2af277bb34c78a5e7c01e029821b918781ac7e51a91bd7c13964318aa1e',
+  'og_about_en.png': '24b589d2af7fed3e65b1efccdd084fe007e3f679aca176cde3bba46e21174723',
+  'og_beyond_ramadan.png': 'cd25495585ff06e0312099eb6148683ead201b6217b9eb7fd05a71a3c87c14b5',
+  'og_calendar.png': 'd84fc5f5852eb2f5cf66960f33a33686e6e40144a997c1e24a5414faeeee9fe5',
+  'og_daily.png': 'd71b3b5b508151435481d9fdba904da04505b2fe1d419851281bacd1c4f7bae2',
+  'og_embed.png': '41f337c61c42c2be1e7039c0d63131cf22a5b4d0ef7f31f9de32a2735f86706c',
+  'og_explore_answers.png': '1e18900fe16d6a4e1aa7fd171d2f0d5e482aaa2c1be356b36838af73f1abf8c1',
+  'og_learning_plans.png': '44f58fcbbdd8812c46c2630ff159b396fb0ec94f874f9c12eada36adaf8afdc5',
+  'og_media.png': '0fe407117bec31f3e15ce6e37ba42bb60aa3f4af8903933b78123c8d6597993e',
+  'og_preparing_for_ramadan.png': '5e27c8afc63614dfa9c2398f0d46d8e592daa58742213ef1211b49d89eaf1a58',
   'og_quran_reader_study_mode_hadith.png':
-    '200e7fbff995072d10d2382ea775fc9bfad9ef87ac52aebd5fce38e317ddc322',
+    'da591c114ce7e840c26eff906fd8edca30bf39e5c744c7cdb68fbb6ad6191147',
   'og_quran_reader_study_mode_layers.png':
-    '0e3ed807a26e04b9138f0d4aeb6a8a6ea07f83971a1661abf1073796ef14bd85',
+    '2302ef99a41615f179dc2106878f8dd7c93e98a8d28273ee26e0bf2a0b0f2401',
   'og_quran_reader_study_mode_lessons.png':
-    'f3cee0fdbba5a11104b1bed7756125cd7629198baeb9638d8b4d7e20ea11edd1',
+    '57ea14b22cd2a0a189e160c22f5c2f39351c7fe47cfbe622f0369518749f6064',
   'og_quran_reader_study_mode_qiraat.png':
-    '5b5731e11541e359c62f7b9a2c95b7254bcdfefcbeb63f35594dcf3d172aecd4',
+    'c9f20bfc6b61ff6ec8b3a1e2d6572bfee87cda911754320ee5bcb6e61fc0a6ec',
   'og_quran_reader_study_mode_reflections.png':
-    '35d74a4851011baf66d1d8634a265ee6af28f86469b071a88bbcc632be00a84c',
+    '0d109b315fb084840803b3a65aff6b7f6a8fe7c03fa7112bdf56c78849c52f11',
   'og_quran_reader_study_mode_related_verses.png':
-    'a3a1edcae770b31eb497ede5d0a33e4d6c3492a86fac854efc06e90b21371b72',
+    '33a09b32e27c881d47d9b5d2f162005c3253a0b0ee1b1d329734a7939b124488',
   'og_quran_reader_study_mode_tafsir.png':
-    'ce4ca40fc343c9cb042f12df8e5f6704363bc263132ba557bfcbd18b1f659bfa',
-  'og_ramadan2026.png': '1013990a839fbbe586e02008ba028ed0171929d3e4e4f35cfa2df9f544d9aeb1',
-  'og_ramadanchallenge.png': 'a5df96d7e8dc57e761d17915adcea9859fd7f73b540ad86ae398f562bf741e55',
-  'og_what_is_ramadan.png': 'e0fd97ba376994d73af24d8244f148de9232e373db2d1e09dd17ac6b45abbd8f',
+    '02be59dd1f1f995dd15f4e7b42d546d60a380bff3d5fd5eba9b52058ccc4fac7',
+  'og_ramadan2026.png': '57be846472a91d34465122b1d6772891179e8bc63637af89441263c233fee140',
+  'og_ramadanchallenge.png': '18294847ad07fdb4cb1a9345f00310782cc9f1270e525f2d471d827ebb87bd17',
+  'og_what_is_ramadan.png': '7970f79175824799df6648e8937717a8e5e8b8a4ebd212fd6e84b9954448413a',
 };
+
+const MAX_PREMADE_TOTAL_BYTES = 3_200_000;
 
 test('generated Open Graph images use the official QDC horizontal logo', () => {
   const logoSource = fs.readFileSync(path.join(process.cwd(), 'src/components/Logo.tsx'), 'utf8');
@@ -61,4 +63,12 @@ test('all reviewed premade Open Graph images retain their approved QDC branding'
     const image = fs.readFileSync(path.join(process.cwd(), 'public/premade', fileName));
     expect(crypto.createHash('sha256').update(image).digest('hex'), fileName).toBe(expectedHash);
   });
+});
+
+test('premade Open Graph images stay within their existing payload budget', () => {
+  const totalBytes = Object.keys(PREMADE_SHA256).reduce((total, fileName) => {
+    return total + fs.statSync(path.join(process.cwd(), 'public/premade', fileName)).size;
+  }, 0);
+
+  expect(totalBytes).toBeLessThanOrEqual(MAX_PREMADE_TOTAL_BYTES);
 });
